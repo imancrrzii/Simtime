191f9603989d7e30a2c0874a1a84dca82f653aa0
Iman Carrazi Syamsidi <99782815+imancrrzii@users.noreply.github.com>
2023-05-21 15:51:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 August 2023";
+
+function countdown() {
+  const newYearDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  console.log(days, hours, minutes, seconds);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+countdown();
+setInterval(countdown, 1000);
